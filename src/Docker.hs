d7e3bb91359793dc58c6920795a933317313f72e
sps-dtank <dtank@spscommerce.com>
2021-06-11 10:59:44 -0400

    docker updates

--- src/Docker.hs
+++ src/Docker.hs
@@ -1,6 +1,8 @@
 module Docker where
 
+import Data.Aeson ((.:))
 import qualified Data.Aeson as Aeson
+import qualified Data.Aeson.Types as Aeson.Types
 import qualified Network.HTTP.Simple as HTTP
 import RIO
 import qualified Socket
@@ -9,6 +11,9 @@ newtype Image = Image Text
   deriving (Eq, Show)
 
 newtype ContainerExitCode = ContainerExitCode Int
+  deriving (Eq, Show)
+
+newtype ContainerId = ContainerId Text
   deriving (Eq, Show)
 
 data CreateContainerOptions = CreateContainerOptions
@@ -21,15 +26,51 @@ imageToText (Image image) = image
 exitCodeToInt :: ContainerExitCode -> Int
 exitCodeToInt (ContainerExitCode code) = code
 
-createContainer :: CreateContainerOptions -> IO ()
+containerIdToText :: ContainerId -> Text
+containerIdToText (ContainerId c) = c
+
+parseResponse :: HTTP.Response ByteString -> (Aeson.Value -> Aeson.Types.Parser a) -> IO a
+parseResponse res parser = do
+  let result = do
+        value <- Aeson.eitherDecodeStrict (HTTP.getResponseBody res)
+        Aeson.Types.parseEither parser value
+  case result of
+    Left e -> throwString e
+    Right status -> pure status
+
+createContainer :: CreateContainerOptions -> IO ContainerId
 createContainer options = do
   manager <- Socket.newManager "/var/run/docker.sock"
-  let body = Aeson.Null
+  let image = imageToText options.image
+  let body =
+        Aeson.object
+          [ ("Image", Aeson.toJSON image),
+            ("Tty", Aeson.toJSON True),
+            ("Labels", Aeson.object [("quad", "")]),
+            ("Cmd", "echo hello"),
+            ("Entrypoint", Aeson.toJSON [Aeson.String "/bin/sh", "-c"])
+          ]
   let req =
         HTTP.defaultRequest
           & HTTP.setRequestManager manager
           & HTTP.setRequestPath "/v1.41/containers/create"
           & HTTP.setRequestMethod "POST"
           & HTTP.setRequestBodyJSON body
+  let parser = Aeson.withObject "createcontainer" $ \o -> do
+        cId <- o .: "Id"
+        pure $ ContainerId cId
+
   res <- HTTP.httpBS req
-  traceShowIO res
+  parseResponse res parser
+
+startContainer :: ContainerId -> IO ()
+startContainer container = do
+  manager <- Socket.newManager "/var/run/docker.sock"
+  let path = "/v1.40/containers/" <> containerIdToText container <> "/start"
+  let req =
+        HTTP.defaultRequest
+          & HTTP.setRequestManager manager
+          & HTTP.setRequestPath (encodeUtf8 path)
+          & HTTP.setRequestMethod "POST"
+
+  void $ HTTP.httpBS req
